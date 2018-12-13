f6cec2ea94682f17f822d9086d5876bad82b3f26
Llenrep <lppernell1998@gmai.com>
2018-12-13 13:04:02 -0500

    updated server.js

--- assets/javascript/server.js
+++ assets/javascript/server.js
@@ -1,3 +1,11 @@
 var http = require("http");
 var fs = require("fs");
-var colors = require("colors")
+var colors = require("colors");
+var express = require("express");
+// var express = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
